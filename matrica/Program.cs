a09bff14448807e7c3e5a9d5fc21517bc92edfe0
Samof_qs56@student.itstep.org <Samof_qs56@student.itstep.org>
2024-11-03 10:47:04 +0200

    book

---
+++ matrica/Program.cs
@@ -0,0 +1,190 @@
+﻿using System;
+
+namespace matrica
+{
+    public class Matrica
+    {
+        private int[,] mass;
+        public int Rows { get; private set; }
+        public int Cols { get; private set; }
+        public Matrica(int rows, int cols)
+        {
+            Rows = rows;
+            Cols = cols;
+            mass = new int[rows, cols];
+        }
+        public Matrica(int[,] array)
+        {
+            Rows = array.GetLength(0);
+            Cols = array.GetLength(1);
+            mass = array;
+        }
+        public void Input()
+        {
+            Console.WriteLine("Введите элементы матрицы:");
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Cols; j++)
+                {
+                    Console.Write($"Индекс [{i},{j}]: ");
+                    mass[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+        public void Output()
+        {
+            Console.WriteLine("Матрица:");
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Cols; j++)
+                {
+                    Console.Write(mass[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+        public int Max()
+        {
+            int max = mass[0, 0];
+            foreach (int value in mass)
+            {
+                if (value > max) max = value;
+            }
+            return max;
+        }
+        public int Min()
+        {
+            int min = mass[0, 0];
+            foreach (int value in mass)
+            {
+                if (value < min) min = value;
+            }
+            return min;
+        }
+        public static Matrica operator +(Matrica a, Matrica b)
+        {
+            if (a.Rows != b.Rows || a.Cols != b.Cols)
+                throw new ArgumentException("Матрицы должны быть одного размера");
+
+            Matrica result = new Matrica(a.Rows, a.Cols);
+            for (int i = 0; i < a.Rows; i++)
+            {
+                for (int j = 0; j < a.Cols; j++)
+                {
+                    result.mass[i, j] = a.mass[i, j] + b.mass[i, j];
+                }
+            }
+            return result;
+        }
+        public static Matrica operator -(Matrica a, Matrica b)
+        {
+            if (a.Rows != b.Rows || a.Cols != b.Cols)
+                throw new ArgumentException("Матрицы должны быть одного размера");
+
+            Matrica result = new Matrica(a.Rows, a.Cols);
+            for (int i = 0; i < a.Rows; i++)
+            {
+                for (int j = 0; j < a.Cols; j++)
+                {
+                    result.mass[i, j] = a.mass[i, j] - b.mass[i, j];
+                }
+            }
+            return result;
+        }
+        public static Matrica operator *(Matrica a, Matrica b)
+        {
+            if (a.Cols != b.Rows)
+                throw new ArgumentException("Количество столбцов первой матрицы должно совпадать с количеством строк второй матрицы");
+
+            Matrica result = new Matrica(a.Rows, b.Cols);
+            for (int i = 0; i < result.Rows; i++)
+            {
+                for (int j = 0; j < result.Cols; j++)
+                {
+                    result.mass[i, j] = 0;
+                    for (int k = 0; k < a.Cols; k++)
+                    {
+                        result.mass[i, j] += a.mass[i, k] * b.mass[k, j];
+                    }
+                }
+            }
+            return result;
+        }
+        public static Matrica operator *(Matrica a, int num)
+        {
+            Matrica result = new Matrica(a.Rows, a.Cols);
+            for (int i = 0; i < a.Rows; i++)
+            {
+                for (int j = 0; j < a.Cols; j++)
+                {
+                    result.mass[i, j] = a.mass[i, j] * num;
+                }
+            }
+            return result;
+        }
+        public static bool operator ==(Matrica a, Matrica b)
+        {
+            if (a.Rows != b.Rows || a.Cols != b.Cols)
+                return false;
+
+            for (int i = 0; i < a.Rows; i++)
+            {
+                for (int j = 0; j < a.Cols; j++)
+                {
+                    if (a.mass[i, j] != b.mass[i, j])
+                        return false;
+                }
+            }
+            return true;
+        }
+        public static bool operator !=(Matrica a, Matrica b)
+        {
+            return !(a == b);
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is Matrica matrica)
+            {
+                return this == matrica;
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return mass.GetHashCode();
+        }
+        public int this[int row, int col]
+        {
+            get { return mass[row, col]; }
+            set { mass[row, col] = value; }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrica m1 = new Matrica(2, 2);
+            m1.Input();
+            m1.Output();
+            Matrica m2 = new Matrica(2, 2);
+            m2.Input();
+            m2.Output();
+            Matrica sum = m1 + m2;
+            Console.WriteLine("Сумма:");
+            sum.Output();
+            Matrica difference = m1 - m2;
+            Console.WriteLine("Отнимание:");
+            difference.Output();
+            Matrica matricaOnmatrica = m1 * m2;
+            Console.WriteLine("Умножение:");
+            matricaOnmatrica.Output();
+            Console.WriteLine("Максимальное значение в матрице: " + m1.Max());
+            Console.WriteLine("Минимальное значение в матрице: " + m2.Min());         
+            Matrica matricaONnumber = m1 * 5;
+            Console.WriteLine("Умножение матрицы на число:");
+            matricaONnumber.Output();
+            Console.WriteLine("Матрицы равны: " + (m1 == m2));      
+        }
+    }
+}
+
---
+++ matrica/books.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.Collections.Generic;
+
+namespace Matrica
+{
+    internal class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public Book(string title, string author)
+        {
+            Title = title;
+            Author = author;
+        }
+        public override string ToString()
+        {
+            return $"'{Title}' Автор: {Author}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Book otherBook)
+            {
+                return Title == otherBook.Title && Author == otherBook.Author;
+            }
+            return false;
+        }
+    }
+    internal class BookList
+    {
+        private List<Book> books = new List<Book>();
+        public void AddNewBook(Book book)
+        {
+            bool bookExists = false;
+            foreach (Book searchBook in books)
+            {
+                if (searchBook.Equals(book))
+                {
+                    bookExists = true;
+                    break;
+                }
+            }
+            if (!bookExists)
+            {
+                books.Add(book);
+                Console.WriteLine($"Книга добавлена: {book}");
+            }
+            else
+            {
+                Console.WriteLine($"Книга уже в списке: {book}");
+            }
+        }
+        public void DeleteBook(Book book)
+        {
+            bool bookExists = false;
+            foreach (Book existingBook in books)
+            {
+                if (existingBook.Equals(book))
+                {
+                    bookExists = true;
+                    books.Remove(existingBook);
+                    Console.WriteLine($"Книга удалена: {book}");
+                    break;
+                }
+            }
+            if (!bookExists)
+            {
+                Console.WriteLine($"Книга не найдена в списке: {book}");
+            }
+        }
+        public bool ContainsBook(Book book)
+        {
+            foreach (Book existingBook in books)
+            {
+                if (existingBook.Equals(book))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public Book this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= books.Count)
+                {
+                    throw new IndexOutOfRangeException("Неверный индекс книги.");
+                }
+                return books[index];
+            }
+        }
+        public int Count
+        {
+            get { return books.Count; }
+        }
+
+        public void PrintAllBooks()
+        {
+            Console.WriteLine("Список книг:");
+            foreach (Book book in books)
+            {
+                Console.WriteLine(book);
+            }
+        }
+        public override int GetHashCode()
+        {
+            return books.GetHashCode();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BookList BookList = new BookList();
+            Book book1 = new Book("1984", "Джордж Оруэлл");
+            Book book2 = new Book("Приключения Тома Сойера", "Марк Твен");
+            Book book3 = new Book("Морской Волк", "Джек Лондон");
+            BookList.AddNewBook(book1);
+            BookList.AddNewBook(book2);
+            BookList.AddNewBook(book3);
+            if (BookList.ContainsBook(book1))
+            {
+                Console.WriteLine("Книга есть в списке.");
+            }
+            else
+            {
+                Console.WriteLine("Книги не найдено.");
+            }
+            BookList.DeleteBook(book2);
+            BookList.PrintAllBooks();
+            Console.WriteLine($"Первая книга по списку: {BookList[0]}");
+        }
+    }
+}
+
